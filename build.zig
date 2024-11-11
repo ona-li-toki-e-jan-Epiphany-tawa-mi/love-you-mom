def796a94515e531d1039c76ce9a3892d333a6e1
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-11-11 12:27:42 -0500

    Initial commit

---
+++ build.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // Default build options.
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Main program.
+    const exe = b.addExecutable(.{
+        .name = "love-you-mom",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    b.installArtifact(exe);
+
+    // Run program command.
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Tell your mom you love her");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,128 @@
+const std = @import("std");
+const fs = std.fs;
+const posix = std.posix;
+const log = std.log;
+const io = std.io;
+const time = std.time;
+const c = std.c;
+const fmt = std.fmt;
+const heap = std.heap;
+const mem = std.mem;
+const process = std.process;
+
+const Allocator = mem.Allocator;
+
+const TtyError = error{
+    NotATty,
+    UnknownSize,
+    WriteFail,
+};
+
+const Tty = struct {
+    file: fs.File,
+    width: usize,
+    height: usize,
+
+    fn init(file: fs.File, allocator: Allocator) TtyError!Tty {
+        var tty = Tty{
+            .file = file,
+            .width = undefined,
+            .height = undefined,
+        };
+
+        if (!posix.isatty(file.handle)) {
+            return TtyError.NotATty;
+        }
+        try tty.update(allocator);
+
+        return tty;
+    }
+
+    fn update(self: *Tty, allocator: Allocator) TtyError!void {
+        getSize: {
+            // First try ioctl.
+            {
+                var winsize = mem.zeroes(posix.winsize);
+                if (-1 != c.ioctl(self.file.handle, posix.T.IOCGWINSZ, &winsize)) {
+                    self.width = winsize.ws_col;
+                    self.height = winsize.ws_row;
+                    break :getSize;
+                }
+            }
+            // Else try COLUMNS and LINES environment variables.
+            getEnv: {
+                var env = process.getEnvMap(allocator) catch break :getEnv;
+                defer env.deinit();
+                const width = env.get("COLUMNS") orelse break :getEnv;
+                const height = env.get("LINES") orelse break :getEnv;
+                self.width = fmt.parseInt(usize, width, 10) catch break :getEnv;
+                self.height = fmt.parseInt(usize, height, 10) catch break :getEnv;
+                break :getSize;
+            }
+            // Else error.
+            return TtyError.UnknownSize;
+        }
+    }
+
+    inline fn write(self: Tty, bytes: []const u8) TtyError!void {
+        self.file.writeAll(bytes) catch return TtyError.WriteFail;
+    }
+
+    inline fn writeFmt(self: Tty, comptime format: []const u8, args: anytype) TtyError!void {
+        fmt.format(self.file.writer(), format, args) catch return TtyError.WriteFail;
+    }
+
+    inline fn home(self: Tty) TtyError!void {
+        try self.write("\x1B[H");
+    }
+
+    inline fn clear(self: Tty) TtyError!void {
+        try self.write("\x1B[2J");
+    }
+};
+
+//inline fn setTermios(tty: Tty, termios: posix.termios) !void {
+//    try posix.tcsetattr(tty.handle, .FLUSH, termios);
+//}
+
+// fn configure(tty: Tty) !posix.termios {
+//     const oldTermios = try posix.tcgetattr(tty.handle);
+//     errdefer setTermios(tty, oldTermios) catch {};
+
+//     var newTermios = oldTermios;
+//     newTermios.lflag.ICANON = false; // Switch to non-canonical mode.
+//     newTermios.lflag.ECHO = false; // Do not echo input.
+//     try setTermios(tty, newTermios);
+//     try tty.writeAll("\x1B[?25l"); // Hide the cursor.
+
+//     return oldTermios;
+// }
+
+// fn reset(tty: Tty, oldTermios: posix.termios) !void {
+//     try setTermios(tty, oldTermios);
+//     try tty.writeAll("\x1B[?25h"); // Show the cursor.
+// }
+
+pub fn main() !void {
+    const allocator = heap.c_allocator;
+    const stdin = io.getStdIn();
+
+    const tty = Tty.init(stdin, allocator) catch |err| switch (err) {
+        TtyError.NotATty => {
+            log.err("stdin is not a tty! You need to use this program with a terminal emulator!", .{});
+            return err;
+        },
+        else => |leftover_err| return leftover_err,
+    };
+
+    //const oldTermios = try configure(tty);
+    //defer reset(tty, oldTermios) catch {};
+
+    try tty.clear();
+    try tty.home();
+    try tty.write("Hellow world!\n");
+    try tty.writeFmt("W: {d}!\n", .{tty.width});
+    try tty.writeFmt("H: {d}!\n", .{tty.height});
+
+    time.sleep(1_000_000_000);
+}
