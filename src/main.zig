9b07e21283eb5980999514ded71090da8f421d9e
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-11-12 21:17:44 -0500

    Split time management code into seperate function run

--- src/main.zig
+++ src/main.zig
@@ -6,17 +6,41 @@ const ttwhy = @import("ttwhy.zig");
 const graphics = @import("graphics.zig");
 
 const Tty = ttwhy.Tty;
-
 const Timer = time.Timer;
+
 const nanosecondsPerSecond = 1_000_000_000;
+const fps = 15;
+const nanosecondsPerFrame: u64 = nanosecondsPerSecond / fps;
+
+fn run(tty: *Tty) !void {
+    try tty.cursor(false);
+
+    var timer = try Timer.start();
+    while (true) {
+        const deltaTime_ns = timer.lap();
+        if (nanosecondsPerFrame > deltaTime_ns) {
+            time.sleep(nanosecondsPerFrame - deltaTime_ns);
+        }
+
+        const deltaTime_s = @as(f64, @floatFromInt(deltaTime_ns)) / nanosecondsPerSecond;
+        graphics.draw(tty, deltaTime_s) catch |err| switch (err) {
+            error.EndOfPlay => break,
+            else => |leftover_err| return leftover_err,
+        };
+        try tty.update();
+    }
+
+    try tty.cursor(true);
+    try tty.resetGraphicModes();
+    try tty.clear();
+    try tty.home();
+    try tty.update();
+}
 
 // TODO added comand line options for saying love you dad.
 // TODO document functions.
 // TODO exit on keypress.
 pub fn main() !void {
-    const fps = comptime 15;
-    const nanosecondsPerFrame: u64 = comptime nanosecondsPerSecond / fps;
-
     const stdin = io.getStdIn();
     var tty = Tty.init(stdin) catch |err| switch (err) {
         ttwhy.Error.NotATty => {
@@ -29,26 +53,5 @@ pub fn main() !void {
         else => |leftover_err| return leftover_err,
     };
 
-    try tty.cursor(false);
-
-    var timer = try Timer.start();
-    while (true) {
-        const deltaTime_ns = timer.lap();
-        if (nanosecondsPerFrame > deltaTime_ns) {
-            time.sleep(nanosecondsPerFrame - deltaTime_ns);
-        }
-
-        const deltaTime_s = @as(f64, @floatFromInt(deltaTime_ns)) / nanosecondsPerSecond;
-        graphics.draw(&tty, deltaTime_s) catch |err| switch (err) {
-            error.EndOfPlay => break,
-            else => |leftover_err| return leftover_err,
-        };
-        try tty.update();
-    }
-
-    try tty.cursor(true);
-    try tty.resetGraphicModes();
-    try tty.clear();
-    try tty.home();
-    try tty.update();
+    try run(&tty);
 }
